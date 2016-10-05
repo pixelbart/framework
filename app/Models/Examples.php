f9a7b0578cbd64c3dadeed4d1608970164c5647a
Kevin Pliester <me@pixelbart.de>
2016-10-05 21:36:17 +0200

    Update Examples.php

--- app/Models/Examples.php
+++ app/Models/Examples.php
@@ -7,16 +7,46 @@
  */
 namespace App\Models;
 
-class Examples
+use App\Core\Model;
+
+class Examples extends Model
 {	
 	/**
-	 * Generate nothing
+	 * Constructor
+	 * Construct the parent model -> mysqli
+	 */
+	public function __construct()
+	{
+		parent::__construct();	
+	}
+	
+	/**
+	 * Get something from your mysql table
 	 *
-	 * @return null
+	 * @return array/string (array if ok string if not ok)
 	 */
 	public function getExamples()
 	{
-		// mysql query or something
-		return null;
+		// Query
+		$example_query = "SELECT example FROM examples";
+		
+		// Get results from query
+		$results = $this->db->query($example_query);
+		
+		// If query is ok return results
+		if($results == true) {					
+			$return = $results->fetch_assoc();
+		}
+		
+		// If not show error 
+		else {
+			$return = srpintf("Error: %s\n", $this->db->error);
+		}
+		
+		// Close Connection
+		$this->db->close();
+		
+		// Return results or error
+		return $return;
 	}
 } // end Class Examples
