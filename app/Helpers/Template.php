defc47e9400bc601483a1ae65650073559c443ef
Kevin Pliester <me@pixelbart.de>
2016-10-05 14:30:16 +0200

    Create Template.php

---
+++ app/Helpers/Template.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Helper Class
+ *
+ * @author  Pixelbart
+ * @version 1.0
+ */
+namespace Helpers;
+
+class Template
+{
+	public static $dir = TEMPLATE_DIR;
+	
+	/** 
+	 * Require the Template Dir
+	 *
+	 * @param string $path
+	 * @param array|string|function $data
+	 * @error array|string|function $error
+	 */
+	public static function render($path, $data = false, $error = false)
+	{		
+		require self::$dir . "$path.php";		
+	}	
+}
