5f6dcf352c5829dfe0d594209fb4bf790e6b53b8
Kevin Pliester <me@pixelbart.de>
2016-10-05 14:44:31 +0200

    Create Examples.php

---
+++ app/Models/Examples.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Examples Model
+ *
+ * @author  Pixelbart
+ * @version 1.0
+ */
+namespace App\Models;
+
+class Examples
+{	
+	/**
+	 * Generate nothing
+	 *
+	 * @return null
+	 */
+	public function getExamples()
+	{
+		// mysql query or something
+		return null;
+	}
+} // end Class Examples
