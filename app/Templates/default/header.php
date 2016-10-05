19094d57a8d716a955f9790b864d561eed9d6b61
Kevin Pliester <me@pixelbart.de>
2016-10-05 14:52:19 +0200

    Create header.php

---
+++ app/Templates/default/header.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html lang="<?php echo BASE_LANG; ?>">
+<head>
+<meta charset="<?php echo BASE_CHARSET; ?>">
+<title><?php echo $data['title']; ?></title>
+</head>
+<body <?php echo $data['body_class']; ?>>
