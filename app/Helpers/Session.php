9d4c339abc2fd4a9af3783eab44fcb13160a236d
Kevin Pliester <me@pixelbart.de>
2016-10-05 14:41:11 +0200

    Update Session.php

--- app/Helpers/Session.php
+++ app/Helpers/Session.php
@@ -12,32 +12,32 @@ class Session
 	protected static $key;
 	protected static $name;
 	
-  /**
-   * Start Session
-   */
+	/**
+	 * Start Session
+	 */
 	public static function start()
 	{
 		session_start();
 	}	
 	
-  /**
-   * Get Session
-   *
-   * @param string $name
-   * @return string $result
-   */
+	/**
+	 * Get Session
+	 *
+	 * @param string $name
+	 * @return string $result
+	 */
 	public static function get($name)
 	{		
 		$result = $_SESSION[$name];		
 		return $result;
 	}	
 	
-  /**
-   * Set a Session
-   *
-   * @param string $name
-   * @param string $key
-   */
+	/**
+	 * Set a Session
+	 *
+	 * @param string $name
+	 * @param string $key
+	 */
 	public static function put($name, $key)
 	{
 		self::$name = $name;
@@ -46,22 +46,22 @@ class Session
 		$_SESSION[self::$name] = self::$key;
 	}
 	
-  /**
-   * Destroy all Sessions
-   */
+	/**
+	 * Destroy all Sessions
+	 */
 	public static function destroy()
 	{
 		session_destroy();
 		$_SESSION = array();		
 	}
 	
-  /**
-   * Check if Session $name exists
-   * If not go to header location $url
-   *
-   * @param string $name
-   * @param string $url
-   */
+	/**
+	 * Check if Session $name exists
+	 * If not go to header location $url
+	 *
+	 * @param string $name
+	 * @param string $url
+	 */
 	public static function check($name, $url)
 	{		
 		if(!isset($_SESSION[$name])) {
