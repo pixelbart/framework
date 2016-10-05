86dea392c784aa6f7c7ee0c12aeb89289b3633b7
Kevin Pliester <me@pixelbart.de>
2016-10-05 14:11:58 +0200

    Create Controller.php

---
+++ app/Core/Controller.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Controller
+ *
+ * @author  Pixeblart
+ * @version 1.0
+ */
+namespace App\Core;
+
+use App\Core\View;
+
+/**
+ * Core controller, all other controllers extend this base controller.
+ */
+abstract class Controller
+{	
+	/**
+	 * View variable to use the view class.
+	 */
+    public $view;
+	
+	/**
+     * On run make an instance of the config class and view class.
+     */
+    public function __construct()
+    {
+        $this->view = new View();
+    }
+}
