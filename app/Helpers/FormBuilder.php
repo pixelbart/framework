19e79a6427614c7d6807446c129870c09b7c1f25
Kevin Pliester <me@pixelbart.de>
2016-10-14 15:05:30 +0200

    Update FormBuilder.php

--- app/Helpers/FormBuilder.php
+++ app/Helpers/FormBuilder.php
@@ -10,13 +10,14 @@ namespace Helpers;
 class FormBuilder
 {
 	/**
-	 * Start the form
+	 * Start form
 	 *
-	 * @param array $atts
-	 * @return string
-	 */
-	public static function start( $atts )
+	 * @param array $atts Attributes
+	 * @retun string $form
+	 */	
+	public static function form( $atts )
 	{		
+		
 		if($atts['action']) {
 			$a = $atts['action'];	
 		} else {
@@ -41,27 +42,28 @@ class FormBuilder
 			$c = '';
 		}
 		
-		$form = '<form action="' . $a . '" method="' . $m . '" class="' . $c . '" id="' . $i . '" enctype="multipart/form-data" />';
-		return $form;		
+		$form  = '<form action="' . $a . '" method="' . $m . '" class="' . $c . '" id="' . $i . '" enctype="multipart/form-data" autocomplete="false" />';
+		
+ 		return $form;		
 	}
 	
 	/**
-	 * Generate a html end form tag
+	 * Close form
 	 *
-	 * @return string
-	 */
-	public static function close()
+	 * @retun string $form
+	 */	
+	public static function form_close()
 	{
 		$form = '</form>';
 		return $form;	
 	}
 	
 	/**
-	 * Generates a html input field
+	 * Generate an html input
 	 *
-	 * @param array $atts
-	 * @return string
-	 */
+	 * @param array $atts Attributes
+	 * @retun string $input
+	 */	
 	public static function input( $atts )
 	{
 		
@@ -69,12 +71,26 @@ class FormBuilder
 			$i = $atts['id'];
 		}
 		
+		if($atts['required'] == true) {
+			$r = 'required="required"';
+		}
+		
 		if($atts['label']) {
 			if($i) {
-				$l = '<label for="' . $i  . '">' . $atts['label'] . '</label>';	
+				if($r) {
+					$l = '<label for="' . $i  . '">' . $atts['label'] . ' <span class="req">*</span></label>';	
+				} else {
+					$l = '<label for="' . $i  . '">' . $atts['label'] . '</label>';						
+				}
 			} else {
-				$l = '<label>' . $atts['label'] . '</label>';				
+				if($r) {
+				$l = '<label>' . $atts['label'] . ' <span class="req">*</span></label>';
+				} else {
+				$l = '<label>' . $atts['label'] . '</label>';
+				}
 			}
+		} else {
+			$l = '';
 		}
 		
 		if($atts['type']) {
@@ -95,6 +111,12 @@ class FormBuilder
 			$c = '';
 		}
 		
+		if($atts['container']) {
+			$con = $atts['container'];
+		} else {
+			$con = '';
+		}
+		
 		if($atts['placeholder']) {
 			$p = $atts['placeholder'];
 		} else {
@@ -107,13 +129,182 @@ class FormBuilder
 			$v = $_POST[$n];
 		}
 		
-		$input  = '<div class="form-group">';
+		$input  = '<div class="form-group ' . $con . '">';
 		$input .= $l;
 		
-		$input .= '<input type="' . $t . '" name="' . $n . '" class="' . $c . '" id="' . $i . '" value="' . $v . '" placeholder="' . $p . '" />';
 		
+		if($t == 'textarea') {
+		
+			$input .= '<textarea name="' . $n . '" class="form-control ' . $c . '" id="' . $i . '"placeholder="' . $p . '" ' . $r . '>' . $v . '</textarea>';
+					
+		} else {
+		
+			$input .= '
+			<input type="' . $t . '" name="' . $n . '" class="form-control ' . $c . '" id="' . $i . '" value="' . $v . '" placeholder="' . $p . '"  ' . $r . ' 
+			 readonly onfocus="this.removeAttribute(\'readonly\');" />';
+		
+		}
+			
 		$input .= '</div>';
 		
 		return $input;
 	}
-} // end Class Session
+	
+	/**
+	 * Generate an progress bar
+	 *
+	 * @param array $atts Attributes
+	 * @retun string $progress
+	 */	
+	public static function progress( $atts )
+	{
+		
+		if($atts['id']) {
+			$i = $atts['id'];
+		}
+		
+		if($atts['class']) {
+			$c = $atts['class'];
+		} else {
+			$c = '';
+		}
+		
+		if($atts['container']) {
+			$con = $atts['container'];
+		} else {
+			$con = '';
+		}
+		
+		if($atts['value']) {
+			$v = $atts['value'];
+		} else {
+			$v = 0;
+		}
+		
+		if($atts['label_left']) {
+			$ll = $atts['label_left'];
+		} else {
+			$ll = '';
+		}
+		
+		if($atts['label_right']) {
+			$lr = $atts['label_right'];
+		} else {
+			$lr = '';
+		}
+		
+		$progress  = '<div class="form-group ' . $con . ' form-progress">';					
+		$progress .= '<span class="label_left">' . $ll . '</span>';					
+		$progress .= '<span class="label_right">' . $lr . '</span>';	
+		$progress .= '<div class="bar' . $c . '" style="width: ' . $v . '%"></div>';		
+		$progress .= '</div>';
+		
+		return $progress;
+	}
+	
+	
+	/**
+	 * Start select
+	 *
+	 * @param array $atts Attributes
+	 * @retun string $select
+	 */
+	public static function select( $atts )
+	{		
+		if($atts['id']) {
+			$i = $atts['id'];	
+		} else {
+			$i = '';
+		}
+		
+		if($atts['required'] == true) {
+			$r = 'required="required"';
+		}
+		
+		if($atts['label']) {
+			if($i) {
+				if($r) {
+					$l = '<label for="' . $i  . '">' . $atts['label'] . ' <span class="req">*</span></label>';	
+				} else {
+					$l = '<label for="' . $i  . '">' . $atts['label'] . '</label>';						
+				}
+			} else {
+				if($r) {
+				$l = '<label>' . $atts['label'] . ' <span class="req">*</span></label>';
+				} else {
+				$l = '<label>' . $atts['label'] . '</label>';
+				}
+			}
+		} else {
+			$l = '';
+		}
+		
+		if($atts['class']) {
+			$c = $atts['class'];			
+		} else {
+			$c = '';
+		}
+		
+		if($atts['container']) {
+			$con = $atts['container'];
+		} else {
+			$con = '';
+		}
+		
+		if($atts['name']) {
+			$n = $atts['name'];
+		} else {
+			$n = '';
+		}
+		
+		$select .= '<div class="form-group ' . $con . '">';	
+		$select .= $l;	
+		
+		$select .= '<select id="' . $a . '" class="' . $c . '" name="' . $n . '">';
+		
+		return $select;		
+	}
+	
+	
+	/**
+	 * Close select
+	 *
+	 * @retun string $select
+	 */
+	public static function select_close()
+	{		
+		$select .= '</select>';
+		
+		$select .= '</div>';
+		
+		return $select;		
+	}
+	
+	/**
+	 * Generate an html option
+	 *
+	 * @param array $atts Attributes
+	 * @retun string $option
+	 */	
+	public static function option( $atts )
+	{
+		
+		if($atts['label']) {
+			$l = $atts['label'];
+		} else {
+			$l = '';
+		}
+		
+		if($atts['value']) {
+			$v = $atts['value'];
+		}
+		
+		if($atts['selected'] == true) {
+			$d = 'selected="selected"';
+		}
+		
+		$option = '<option value="' . $v . '" ' . $d . '>' . $l . '</option>';
+		
+		return $option;
+	}
+}
